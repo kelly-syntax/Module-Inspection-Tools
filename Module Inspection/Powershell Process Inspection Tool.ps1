879282a0923be30da1d91083a4dc95adfe8d397c
kelly-syntax <kellykels@live.com>
2023-01-31 20:40:01 -0800

    Add files via upload

---
+++ Module Inspection/Powershell Process Inspection Tool.ps1
@@ -0,0 +1,32 @@
+#This script tells us what dlls (and other executables) are being used in a given process.
+
+
+$processId = Read-Host -prompt 'Please enter a valid Process ID (PID)'
+
+
+
+$printHdr = $true
+
+$modules = get-process -id $processId | select -expand Modules -ea silentlycontinue
+
+foreach ($module in $modules) {
+
+$actualBase = $module.BaseAddress.ToInt64()
+
+if ($actualBase -ne $imageBase) {
+
+if ($printHdr) {
+
+"`nModule Relocations in Process Id: $processId"
+
+"--------------------------------------------"
+
+$printHdr = $false
+
+}
+
+"{0}`n Loaded at: 0x{2:X8}" -f $module.Filename, $imageBase, $actualBase
+
+}
+
+}
